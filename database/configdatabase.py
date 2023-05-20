1756b5aa303d611b2d810ebd0b1425e2517d4dc7
Desgasa <122233531+Desgasa@users.noreply.github.com>
2023-05-20 13:27:49 +0300

    Update configdatabase.py

--- database/configdatabase.py
+++ database/configdatabase.py
@@ -4,11 +4,11 @@ from mysql.connector import Error
 def connect():
     global dbconn 
     dbconn = mysql.connector.connect(
-    host = "localhost",
-    port = 3306, 
-    user = "root",
+    host = "yourhost",
+    port = yourport, 
+    user = "youruser",
     password = "",
-    database = "test2python"
+    database = "yourdatabase"
 )
 
 connect()
